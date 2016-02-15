bc5632a30d144792a1b1488618e04d3da79f7dcf
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-02-15 23:55:08 +0800

    First commit

---
+++ EhouarnPerret.CSharp.TestDome.Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿// File: Program.cs
+// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Rights Reserved.
+using System;
+
+namespace EhouarnPerret.CSharp.TestDome.Sandbox
+{
+    public static class Program
+    {
+        public static void Main(params String[] args)
+        {
+        }
+    }
+}
---
+++ EhouarnPerret.CSharp.TestDome.Sandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// File: AssemblyInfo.cs
+// Author: Ehouarn Perret
+// Email: ehouarn.perret@art-logics.com
+// Company: ART logics
+// Website: http://www.art-logics.com
+// Copyright: ART logics © All Rights Reserved.
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("EhouarnPerret.CSharp.TestDome.Sandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Ehouarn Perret")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
